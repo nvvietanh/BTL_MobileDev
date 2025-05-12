f38d188740cadd168c2051091a70be17ecee64c2
chinh.dv@tinasoft.com.vn <chinh27062003@gmail.com>
2025-05-12 23:36:38 +0700

    update-share-fragment

--- app/src/main/java/com/example/appdocbao/Fragment/ShareFragment.java
+++ app/src/main/java/com/example/appdocbao/Fragment/ShareFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -11,13 +12,27 @@ import androidx.fragment.app.Fragment;
 
 import com.example.appdocbao.R;
 
+
 public class ShareFragment extends Fragment {
 
-    @Nullable
+    ImageView backarrow;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        // Chỉ inflate layout rỗng hoặc có nội dung tạm thời
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_share, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        backarrow = view.findViewById(R.id.backarrow);
+        backarrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                NewsFragment newsFragment = new NewsFragment();
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.frame_layout,newsFragment).commit();
+            }
+        });
     }
 }
